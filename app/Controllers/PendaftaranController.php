b97bc1898014d834421b8be4657c447b4fed239c
DelaSurindra <75169954+DelaSurindra@users.noreply.github.com>
2021-10-19 23:26:10 +0700

    mengurangi variabel

--- app/Controllers/PendaftaranController.php
+++ app/Controllers/PendaftaranController.php
@@ -39,26 +39,26 @@ class PendaftaranController extends BaseController {
 			session()->setFlashdata('error', 'Organisasi Sudah Terdaftar.');
 			return redirect()->back();
                 } else {
-                        $file_surat_pemberitahuan = $this->request->getFile("surat_pemberitahuan");
-                        $file_name_surat_pemberitahuan = $file_surat_pemberitahuan->getName();
+                        $file_surat_berita = $this->request->getFile("surat_pemberitahuan");
+                        $file_name_srt_berita = $file_surat_berita->getName();
                         $file_sk_kemenkumham = $this->request->getFile("sk_kemenkumham");
-                        $file_name_sk_kemenkumham = $file_sk_kemenkumham->getName();
+                        $file_name_sk_kmnkmhm = $file_sk_kemenkumham->getName();
                         $file_sk_kepengurusan = $this->request->getFile("sk_kepengurusan");
-                        $file_name_sk_kepengurusan = $file_sk_kepengurusan->getName();
+                        $file_name_sk_kpngrsn = $file_sk_kepengurusan->getName();
                         $file_akte = $this->request->getFile("akte");
                         $file_name_akte = $file_akte->getName();
                         $file_ad_art = $this->request->getFile("ad_art");
                         $file_name_ad_art = $file_ad_art->getName();
                         $file_program_kerja = $this->request->getFile("program_kerja");
-                        $file_name_program_kerja = $file_program_kerja->getName();
+                        $file_name_prog_kerja = $file_program_kerja->getName();
                         $file_biodata = $this->request->getFile("biodata");
                         $file_name_biodata = $file_biodata->getName();
                         $file_ktp = $this->request->getFile("ktp");
                         $file_name_ktp = $file_ktp->getName();
                         $file_formulir = $this->request->getFile("formulir");
                         $file_name_formulir = $file_formulir->getName();
-                        $file_surat_keterangan = $this->request->getFile("surat_keterangan");
-                        $file_name_surat_keterangan = $file_surat_keterangan->getName();
+                        $file_srt_keterangan = $this->request->getFile("surat_keterangan");
+                        $file_name_srt_ktrngn = $file_srt_keterangan->getName();
                         $file_npwp = $this->request->getFile("npwp");
                         $file_name_npwp = $file_npwp->getName();
                         $file_foto = $this->request->getFile("foto");
@@ -103,29 +103,29 @@ class PendaftaranController extends BaseController {
                         
                 $syarat = [
                         "nama_organisasi" => $this->request->getVar("nama_organisasi"),
-                        "surat_pemberitahuan" => $file_name_surat_pemberitahuan,
-                        "sk_kemenkumham" => $file_name_sk_kemenkumham,
-                        "sk_kepengurusan" => $file_name_sk_kepengurusan,
+                        "surat_pemberitahuan" => $file_name_srt_berita,
+                        "sk_kemenkumham" => $file_name_sk_kmnkmhm,
+                        "sk_kepengurusan" => $file_name_sk_kpngrsn,
                         "akte" => $file_name_akte,
                         "ad_art" => $file_name_ad_art,
-                        "program_kerja" => $file_name_program_kerja,
+                        "program_kerja" => $file_name_prog_kerja,
                         "biodata" => $file_name_biodata,
                         "ktp" => $file_name_ktp,
                         "formulir" => $file_name_formulir,
-                        "surat_keterangan" => $file_name_surat_keterangan,
+                        "surat_keterangan" => $file_name_srt_ktrngn,
                         "npwp" => $file_name_npwp,
                         "foto" => $file_name_foto,
                         "keabsahan" => $file_name_keabsahan
                         ];
         
-                        $file_surat_pemberitahuan->move('document/pendaftaran/' . 
-                        $this->request->getVar("nama_organisasi"), "Surat Pemberitahuan - " . $file_name_surat_pemberitahuan);
+                        $file_surat_berita->move('document/pendaftaran/' . 
+                        $this->request->getVar("nama_organisasi"), "Surat Pemberitahuan - " . $file_name_srt_berita);
                 
                         $file_sk_kemenkumham->move('document/pendaftaran/' . 
-                        $this->request->getVar("nama_organisasi"), "SK Kemenkumham - " . $file_name_sk_kemenkumham);
+                        $this->request->getVar("nama_organisasi"), "SK Kemenkumham - " . $file_name_sk_kmnkmhm);
                 
                         $file_sk_kepengurusan->move('document/pendaftaran/' . 
-                        $this->request->getVar("nama_organisasi"), "SK Kepengurusan - " . $file_name_sk_kepengurusan);
+                        $this->request->getVar("nama_organisasi"), "SK Kepengurusan - " . $file_name_sk_kpngrsn);
                 
                         $file_akte->move('document/pendaftaran/' . 
                         $this->request->getVar("nama_organisasi"), "Akte - " . $file_name_akte);
@@ -134,7 +134,7 @@ class PendaftaranController extends BaseController {
                         $this->request->getVar("nama_organisasi"), "AD & ART - " . $file_name_ad_art);
                 
                         $file_program_kerja->move('document/pendaftaran/' . 
-                        $this->request->getVar("nama_organisasi"), "Program Kerja - " . $file_name_program_kerja);
+                        $this->request->getVar("nama_organisasi"), "Program Kerja - " . $file_name_prog_kerja);
                 
                         $file_biodata->move('document/pendaftaran/' . 
                         $this->request->getVar("nama_organisasi"), "Biodata - " . $file_name_biodata);
@@ -145,8 +145,8 @@ class PendaftaranController extends BaseController {
                         $file_formulir->move('document/pendaftaran/' . 
                         $this->request->getVar("nama_organisasi"), "Formulir - " . $file_name_formulir);
                 
-                        $file_surat_keterangan->move('document/pendaftaran/' . 
-                        $this->request->getVar("nama_organisasi"), "Surat Keterangan - " . $file_name_surat_keterangan);
+                        $file_srt_keterangan->move('document/pendaftaran/' . 
+                        $this->request->getVar("nama_organisasi"), "Surat Keterangan - " . $file_name_srt_ktrngn);
                 
                         $file_npwp->move('document/pendaftaran/' . 
                         $this->request->getVar("nama_organisasi"), "NPWP - " . $file_name_npwp);
